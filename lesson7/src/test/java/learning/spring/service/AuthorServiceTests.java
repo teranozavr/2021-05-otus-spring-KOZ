2ac8c792660a319c04e18195e31a2371a0ae0f93
Козырев Илья Михайлович <imkozyrev@sberbank.ru>
2021-10-10 19:37:02 +0300

    Изменены тесты AuthorServiceTests с использованием Mock

--- lesson7/src/test/java/learning/spring/service/AuthorServiceTests.java
+++ lesson7/src/test/java/learning/spring/service/AuthorServiceTests.java
@@ -1,73 +1,108 @@
 package learning.spring.service;
 
-import learning.spring.dao.AuthorDaoJdbc;
-import org.junit.jupiter.api.BeforeEach;
+import learning.spring.dao.AuthorDao;
+import learning.spring.domain.Author;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.Description;
-import org.springframework.context.annotation.Import;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @DisplayName("Тесты AuthorService")
-@JdbcTest
-@Import({AuthorDaoJdbc.class})
+@ExtendWith(MockitoExtension.class)
 public class AuthorServiceTests {
-    private AuthorService authorService;
-    private static final String NAME = "name";
-    private static final String SUR_NAME = "surname";
-    private static final String MIDDLE_NAME = "middlename";
+
     private static final String EXIST_NAME = "Роджер";
     private static final String EXIST_SUR_NAME = "Желязны";
     private static final String EXIST_MIDDLE_NAME = "Джозеф";
     private static final String NOT_EXIST_NAME = "Роджер2";
     private static final String NOT_EXIST_SUR_NAME = "Желязны2";
     private static final String NOT_EXIST_MIDDLE_NAME = "Джозеф2";
+    private static final Author existAuthor = new Author(1L, EXIST_NAME, EXIST_SUR_NAME, EXIST_MIDDLE_NAME);
+    private static final Author notExistAuthor = new Author(1L, NOT_EXIST_NAME, NOT_EXIST_SUR_NAME, NOT_EXIST_MIDDLE_NAME);
 
-    @Autowired
-    private AuthorDaoJdbc authorDaoJdbc;
+    @Mock
+    private AuthorDao authorDaoMock;
+    @InjectMocks
+    private AuthorService authorService;
 
-    @BeforeEach
-    private void init(){
-        authorService = new AuthorService(authorDaoJdbc);
+    public static String getExistMiddleName() {
+        return EXIST_MIDDLE_NAME;
     }
 
     @DisplayName("Выводит ФИО автора")
     @Test
     public void printAuthorInfoTest(){
+        when(authorDaoMock.getById(anyLong())).thenReturn(existAuthor);
         assertEquals(authorService.printAuthorInfo(1L), "Автор: Роджер Джозеф Желязны\n");
     }
 
     @DisplayName("Создает автора")
     @Test
     public void createAuthorTest(){
-        assertFalse(authorService.isAuthorExists(NAME, SUR_NAME, MIDDLE_NAME));
-        authorService.createAuthor(NAME, SUR_NAME, MIDDLE_NAME);
-        assertTrue(authorService.isAuthorExists(NAME, SUR_NAME, MIDDLE_NAME));
-        authorService.createAuthor(NAME, SUR_NAME, MIDDLE_NAME);
+        authorService.createAuthor(EXIST_NAME, EXIST_SUR_NAME, EXIST_MIDDLE_NAME);
+        verify(authorDaoMock, times(1)).createAuthor(eq(existAuthor));
     }
 
     @DisplayName("Получает ID автора по ФИО")
     @Test
     public void getAuthorIdTest(){
+        when(authorDaoMock.getByName(eq(existAuthor))).thenReturn(existAuthor);
+
         assertNotNull(authorService.getAuthorId(EXIST_NAME, EXIST_SUR_NAME, EXIST_MIDDLE_NAME));
+        verify(authorDaoMock, times(1)).getByName(eq(existAuthor));
+    }
+
+    @DisplayName("Получает ID автора по ФИО (негативный тест)")
+    @Test
+    public void getAuthorIdNegativeTest(){
+        when(authorDaoMock.getByName(eq(notExistAuthor))).thenThrow(EmptyResultDataAccessException.class);
+
         assertNull(authorService.getAuthorId(NOT_EXIST_NAME, NOT_EXIST_SUR_NAME, NOT_EXIST_MIDDLE_NAME));
+
+        verify(authorDaoMock, times(1)).getByName(eq(notExistAuthor));
     }
 
     @DisplayName("Проверяет существование автора")
     @Test
     public void isAuthorExistsTest(){
+        when(authorDaoMock.getByName(eq(existAuthor))).thenReturn(existAuthor);
+
         assertTrue(authorService.isAuthorExists(EXIST_NAME, EXIST_SUR_NAME, EXIST_MIDDLE_NAME));
+    }
+
+    @DisplayName("Проверяет существование автора (негативный тест)")
+    @Test
+    public void isAuthorExistsNegativeTest(){
+        when(authorDaoMock.getByName(eq(notExistAuthor))).thenThrow(EmptyResultDataAccessException.class);
+
         assertFalse(authorService.isAuthorExists(NOT_EXIST_NAME, NOT_EXIST_SUR_NAME, NOT_EXIST_MIDDLE_NAME));
     }
 
     @DisplayName("Получает Id нового, или существующего автора")
     @Test
     public void addAuthorIdTest(){
-        assertNotNull(authorService.addAuthorId(EXIST_NAME, EXIST_SUR_NAME, EXIST_MIDDLE_NAME));
-        assertNotNull(authorService.addAuthorId(NOT_EXIST_NAME, NOT_EXIST_SUR_NAME, NOT_EXIST_MIDDLE_NAME));
+        when(authorDaoMock.getByName(eq(existAuthor))).thenReturn(existAuthor);
+
+        assertEquals(authorService.addAuthorId(EXIST_NAME, EXIST_SUR_NAME, EXIST_MIDDLE_NAME), existAuthor.getId());
+        verify(authorDaoMock, times(0)).createAuthor(eq(existAuthor));
+    }
+
+    @DisplayName("Получает Id нового, или существующего автора (негативный тест)")
+    @Test
+    public void addAuthorIdNegativeTest(){
+        when(authorDaoMock.getByName(eq(notExistAuthor))).thenThrow(EmptyResultDataAccessException.class);
+
+        assertNull(authorService.addAuthorId(NOT_EXIST_NAME, NOT_EXIST_SUR_NAME, NOT_EXIST_MIDDLE_NAME));
+        verify(authorDaoMock, times(1)).createAuthor(eq(notExistAuthor));
+
     }
 
 }
