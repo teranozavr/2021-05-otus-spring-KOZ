a4933f71f6049be7c9ea25011387008b9c3dfa9f
Козырев Илья Михайлович <imkozyrev@sberbank.ru>
2021-11-30 21:17:15 +0300

    lesson29 init

---
+++ lesson29/caterpillar/src/main/java/learning/spring/caterpillar/CaterpillarApplication.java
@@ -0,0 +1,13 @@
+package learning.spring.caterpillar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaterpillarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaterpillarApplication.class, args);
+	}
+
+}
---
+++ lesson29/caterpillar/src/test/java/learning/spring/caterpillar/CaterpillarApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.caterpillar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaterpillarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
